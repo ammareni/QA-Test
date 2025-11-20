abcb96a8e9120e109ab07fc2f4812416110b49f8
ammareni <157006984+ammareni@users.noreply.github.com>
2025-11-20 12:05:43 +0800

    Update nedashomepage.spec.ts

--- tests/nedashomepage.spec.ts
+++ tests/nedashomepage.spec.ts
@@ -1,6 +1,10 @@
+
 import { test, expect } from '@playwright/test';
 
 test('homepage loads and shows correct title', async ({ page }) => {
   await page.goto('https://www.assessment.nedasmy.com/');
   await expect(page).toHaveTitle(/NxBAS/);
+
+  // Capture screenshot
+  await page.screenshot({ path: 'screenshots/homepage.png' });
 });
